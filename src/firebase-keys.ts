b9626f2502da11438ba58d4652032219bbbb4d4e
Manish Aneja <manishekaneja@gmail.com>
2021-06-28 11:38:40 +0530

    Improve: Improve manifest.json and icon changes

--- src/firebase-keys.ts
+++ src/firebase-keys.ts
@@ -1,12 +1,12 @@
 // Add firebase configuration here
 const firebaseKeys = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-  };
-  
-  export { firebaseKeys };
+  apiKey: 'AIzaSyDVMoTVwAVWFDhlQ6-vOWNFXny6Ia5CWgA',
+  authDomain: 'portal-fdf7a.firebaseapp.com',
+  databaseURL: 'https://portal-fdf7a.firebaseio.com',
+  projectId: 'portal-fdf7a',
+  storageBucket: 'portal-fdf7a.appspot.com',
+  messagingSenderId: '1062567198731',
+  appId: '1:1062567198731:web:f797b9370525b3f1c2c70e',
+};
+
+export {firebaseKeys};
